d2f691acc3c7495424e983db844a10a7eac4a4fc
Yogesh <yogeshsuresh28@gmail.com>
2022-11-16 09:45:10 +0530

    Switch between scenes completed

--- MyGame/Assets/SceneChanger.cs
+++ MyGame/Assets/SceneChanger.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public Button button;
+    public string SceneName;
+
     void Start()
     {
-        
+        button.onClick.AddListener(OnButtonClick);
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnButtonClick()
     {
-        
+        Debug.Log(SceneName + " Clicked !");
+        SceneManager.LoadScene(SceneName);
     }
 }
